225d319958cb2056a1441f3c2bc6a0e12bb6e555
thaidui123 <vothongthai02012006@gmail.com>
2024-03-04 11:14:02 +0700

    Add project files.

---
+++ bai8/bai8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int so, tongle = 0;
+	cout << "nhap so";
+	cin >> so;
+	for (int i = 0; i <= so; i++) {
+		if (i % 2 != 0)
+		{
+			cout << i << "  ";
+			tongle = tongle + 1;
+		}
+	}
+	cout << "\n tong cac so le tu 0 den" << so << "la" << tongle;
+	return 0;
+}
